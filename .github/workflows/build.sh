df6c22d075e0f383b374ff9a1f0b9c423f5ab562
Scott Phillips <polygonguru@gmail.com>
2025-12-18 19:57:25 +0000

    fix route

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,7 +1,7 @@
 # TODO - Ensure openapi tools are installed:
 npm install @openapitools/openapi-generator-cli -g
 openapi-generator-cli generate \
-    -i ../../ncentral-api.json \
+    -i ../../cloudradial-api.json \
     -g go -o ../../ \
     -c config.yaml \
     --skip-validate-spec \
